d67f6d98ae5338e906ae7b6b1b507cacff98abcf
Robert Holmes <rwholmes1@gmail.com>
2014-05-12 19:16:56 -0700

    Change php contact form

---
+++ mail.php
@@ -0,0 +1,32 @@
+<?php
+	$name = $_POST['name'];
+	$email= $_POST['email'];
+	$message = $_POST['message'];
+	$company = $_POST['company'];
+ 
+	$subject = "Twenty App - Contact Form Inquiry";
+	
+	$headers = 'From: The Twenty Team <twentyapplication@gmail.com>' . "\r\n";  
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    
+    $emailbody = "<p>You have recieved a new message from the enquiries form on your website.</p> 
+                  <p><strong>Name: </strong> {$name} <br /><strong>Company: </strong> {$company} <br /><strong>Email Address: </strong> {$email} </p>
+                  <p><strong>Message: </strong><br /><br />{$message} </p>";    
+	
+	
+	
+	if ($_POST['submit']) {
+    	if ($name != '' && $email != '') {
+    		if (mail ("rwholmes1@gmail.com", $subject, $emailbody, $headers)) { 
+        		header('Location: index.html');
+		    }
+		    else { 
+        		echo '<p>Something went wrong, go back and try again!</p>'; 
+	    	}
+    	}
+    	else {
+        	echo '<p>You need to fill in all required fields!!</p>';
+	    }
+    }
+
+?>
